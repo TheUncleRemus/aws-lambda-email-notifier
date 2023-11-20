b2be3070eaae1737e69d45af93bf379eb328e340
Tiziano Catalano <syd.floyd.tc@gmail.com>
2023-11-20 22:10:08 +0100

    added default path value for StonTemplate

--- src/notifier/emailsender/template.py
+++ src/notifier/emailsender/template.py
@@ -1,4 +1,7 @@
 from jinja2 import FileSystemLoader, Environment
+from aws_lambda_powertools import Logger
+
+logger = Logger()
 
 
 class Singleton(type):
@@ -18,12 +21,12 @@ class StonTemplate(metaclass=Singleton):
     This class represents the email common templates for lambda.
     """
 
-    def __init__(self, path):
+    def __init__(self, path=None):
         """
 
         :param path:
         """
-        templates = FileSystemLoader(path)
+        templates = FileSystemLoader(path if path is not None else "src/resources")
         self.__env = Environment(loader=templates)
 
     def get_template(self, filename):
